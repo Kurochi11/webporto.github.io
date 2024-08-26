bf2307562ae968779d69008df8d1f1c854b526f3
A.H.D <96622779+Kurochi11@users.noreply.github.com>
2024-08-26 14:29:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-// script.js
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
-  });
-});
+let menuicon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
